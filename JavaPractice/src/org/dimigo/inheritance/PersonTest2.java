830e5771d56ce95c0c305b908930318165b82a4e
WP152317 <yag0308@naver.com>
2017-05-10 12:28:26 +0900

    Update PersonTest2.java

--- JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -1,1 +1,26 @@
 
+package org.dimigo.inheritance;
+
+/**
+ * Created by pc on 2017-05-10.
+ */
+public class PersonTest2 {
+    private static void greeting(Person p){
+        p.sayHello();
+        p.sayBye();
+    }
+    public static void main(String args[]){
+
+        Person[] p=new Person[]{
+                new Person("Tom"),
+                new Korean("홍길동"),
+                new Japanese("다나카"),
+                new Chinese("왕밍"),
+        };
+        for(int i=0;i<4;i++){
+            System.out.println(p[i]);
+            greeting(p[i]);
+            System.out.println();
+        }
+    }
+}
