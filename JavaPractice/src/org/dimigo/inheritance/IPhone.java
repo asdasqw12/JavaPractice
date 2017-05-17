e9f9d6d1ec8acbb4510616734ff1f52b9dc49aa2
WP152317 <yag0308@naver.com>
2017-05-17 12:03:51 +0900

    Update IPhone.java

--- JavaPractice/src/org/dimigo/inheritance/IPhone.java
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -3,17 +3,17 @@ package org.dimigo.inheritance;
 /**
  * Created by pc on 2017-05-12.
  */
-public class Galaxy extends SmartPhone {
-    public Galaxy() {
+public class IPhone extends SmartPhone {
+    public IPhone() {
     }
 
-    public Galaxy(String model, String company, int price) {
+    public IPhone(String model, String company, int price) {
         super(model, company, price);
     }
     public void pay(){
-        System.out.println("삼성 페이로 결제합니다.");
+        System.out.println("애플 페이로 결제합니다.");
     }
-    public void useWirelessCharging(){
-        System.out.println("무선 충전 기능을 이용합니다.");
+    public void useAirDrop(){
+        System.out.println("AirDrop 기능을 사용합니다.");
     }
 }
